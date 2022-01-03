3f6dabed9f4f6cbf9e9db474912f6dbeba3f0ebc
3599josephw <3599@holbertonschool.com>
2022-01-03 10:38:15 -0800

    negative correction

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-print("Last digit of", number, "is", last_digit, end=" ")
-if last_digit > 5:
-        print("and is greater than 5")
-elif last_digit == 0:
+pos = abs(number) % 10
+neg = -pos
+print("Last digit of", number, "is", pos if number >= 0 else neg, end=" ")
+if pos == 0:
         print("and is 0")
-elif last_digit < 6 and last_digit != 0:
+elif pos > 5 and number > 0:
+        print("and is greater than 5")
+else:
         print("and is less than 6 and not 0")
