f2dc3aaa8d917b311459af23de3e8b1083eb0f54
3599josephw <3599@holbertonschool.com>
2022-01-06 12:27:14 -0800

    list null check

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -4,6 +4,9 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
 
+    if not my_list:
+        return None
+
     my_list.remove(idx)
 
     return my_list
