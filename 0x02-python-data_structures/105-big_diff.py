708cb92f9b1392fcb9e69cb79163b96313b03e56
3599josephw <3599@holbertonschool.com>
2022-01-07 11:49:51 -0800

    105

--- 0x02-python-data_structures/105-big_diff.py
+++ 0x02-python-data_structures/105-big_diff.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
 def big_diff(my_list=[]):
 
-    if len(my_list) <= 1:
-        return 0
-
-    largest = my_list[0]
-    smallest = my_list[0]
-
-    for i in my_list:
-        if i > largest:
-            largest = i
-        elif i < smallest:
-            smallest = i
-
-    diff = largest - smallest
-
-    return diff
+    return sorted(my_list)[(len(my_list) - 1)] - sorted(my_list)[0]
