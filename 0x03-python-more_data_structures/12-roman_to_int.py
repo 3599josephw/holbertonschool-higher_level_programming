140ce1ea9945ca727279f0a6b9d03f2a048155a3
3599josephw <3599@holbertonschool.com>
2022-01-10 14:00:29 -0800

    more comprehensive dictionary

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    my_dict = {'I': 1, 'V': 5, 'X': 10, 'IX': 9, 'XX': 20, 'XXX': 30, 'XL': 40, 'L': 50, 'XC': 90, 'DCC': 700, 'C': 100, 'D': 500}
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'IX': 9, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'D': 500, 'CM': 900, 'M': 1000}
 
     result = 0
 
