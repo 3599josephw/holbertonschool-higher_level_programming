e755fa94c18bad2fa505a7cd3e9e81fdf08f8355
Joseph W <3599@holbertonschool.com>
2022-01-12 10:22:23 -0600

    none check

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
 
     if a_dictionary is None:
         return None
+    elif len(a_dictionary) is 0:
+        return None
 
     sort_dict = dict(sorted(a_dictionary.items(), key=lambda x: x[1]))
 
