3b49d581c6daccaadfc033240412149017334bf5
Joseph W <3599@holbertonschool.com>
2022-01-12 10:25:09 -0600

    none check

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
 
-    if a_dictionary is None:
-        return None
-    elif len(a_dictionary) is 0:
-        return None
-
-    sort_dict = dict(sorted(a_dictionary.items(), key=lambda x: x[1]))
-
-    best_key = next(reversed(sort_dict.keys()))
-
-    return best_key
+    if a_dictionary:
+        return max(a_dictionary, key=lambda x: a_dictionary[x])
