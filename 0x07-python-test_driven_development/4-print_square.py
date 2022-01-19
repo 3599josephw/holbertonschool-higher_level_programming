b1a0f6358aa643e9a8d43c6c929a3253fd304464
Joseph W <3599@holbertonschool.com>
2022-01-19 09:52:34 -0600

    size 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,11 +12,8 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size is 0:
-            print()
-    else:
-        for i in range(size):
-            for j in range(size):
-                print("#", end="")
-                if j is size - 1:
-                    print("")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+            if j is size - 1:
+                print("")
