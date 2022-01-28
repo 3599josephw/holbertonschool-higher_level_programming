41eeeffb9983cb26761d7db5ebb330d0c35a8d39
Joseph W <3599@holbertonschool.com>
2022-01-28 16:08:34 -0600

    delete half the file

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,7 +19,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_pep8_test_base(self):
+    def test_pep8_test_rectangle(self):
         """
         Test that tests/test_models/test_rectangle.py is pep8 compliant
         """
@@ -107,65 +107,3 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         """tests the area"""
         self.assertEqual(self.r3.area(), 35)
-
-    def test_display(self):
-        """tests display"""
-        self.assertEqual(self.r4.display(), None)
-
-    def test_str(self):
-        """tests str"""
-        self.assertEqual(str(self.r5), "[Rectangle] (12) 2/1 - 4/6")
-
-    def test_update(self):
-        """tests update function"""
-        self.r6.update(89)
-        self.assertEqual(self.r6.id, 89)
-        self.r6.update(89, 2, 3, 4, 5)
-        self.assertEqual(self.r6.width, 2)
-        self.assertEqual(self.r6.height, 3)
-        self.assertEqual(self.r6.x, 4)
-        self.assertEqual(self.r6.y, 5)
-        self.r6.update(width=10, height=9, x=8, y=7, id=11)
-        self.assertEqual(self.r6.width, 10)
-        self.assertEqual(self.r6.height, 9)
-        self.assertEqual(self.r6.x, 8)
-        self.assertEqual(self.r6.y, 7)
-        self.assertEqual(self.r6.id, 11)
-
-    def test_to_dictionary(self):
-        """tests to dictionary"""
-        r7_dict = self.r7.to_dictionary()
-        self.assertEqual(r7_dict, {'x': 1, 'y': 9, 'id': 1,
-                                   'height': 2, 'width': 10})
-        self.assertEqual(type(r7_dict), dict)
-        self.assertEqual(Rectangle.to_json_string([]), "[]")
-        self.assertEqual(Rectangle.to_json_string(None), "[]")
-
-    def test_save_to_file(self):
-        """tests save_to_file"""
-        Rectangle.save_to_file([self.r8, self.r9])
-        with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.read(), '[{"id": 3, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 4,\
- "width": 2, "height": 4, "x": 0, "y": 0}]')
-
-    def test_create(self):
-        """test create"""
-        r10_dict = self.r10.to_dictionary()
-        r11 = Rectangle.create(**r10_dict)
-        self.assertEqual(r11.id, 5)
-        self.assertEqual(r11.width, 3)
-        self.assertEqual(r11.height, 5)
-        self.assertEqual(r11.x, 1)
-        self.assertEqual(r11.y, 0)
-        self.assertNotEqual(self.r10, r11)
-
-    def test_load_from_file(self):
-        """tests load_from_file"""
-        list_rectangles_input = [self.r12, self.r13]
-        Rectangle.save_to_file(list_rectangles_input)
-        list_rectangles_output = Rectangle.load_from_file()
-        self.assertEqual(str(list_rectangles_output[0]),
-                         '[Rectangle] (6) 2/8 - 10/7')
-
-if __name__ == '__main__':
-    unittest.main()
