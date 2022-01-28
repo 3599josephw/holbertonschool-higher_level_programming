0c4180a172b78273de04280c6be4027e0874cd79
Joseph W <3599@holbertonschool.com>
2022-01-28 16:13:49 -0600

    base & rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,112 +1,95 @@
 #!/usr/bin/python3
-from models.base import Base
 import unittest
+from models.base import Base
 import pep8
-"""Test for class base"""
+"""Unittests for Base class
+"""
 
 
-class BaseTest(unittest.TestCase):
-    """Test for class base"""
-
-    @classmethod
-    def setUpClass(cls):
-        """Setting up class"""
-        Base.clear()
-
-        cls.b1 = Base()
-        cls.b2 = Base()
-        cls.b3 = Base()
-        cls.b4 = Base(5)
-        cls.b5 = Base(-5)
-        cls.b6 = Base(42)
-        cls.b7 = Base()
-        cls.b8 = Base(None)
-
-    @classmethod
-    def tearDownClass(cls):
-        """Tearing down class"""
-        del cls.b1
-        del cls.b2
-        del cls.b3
-        del cls.b4
-        del cls.b5
-        del cls.b6
-        del cls.b7
-        del cls.b8
-
-    def test_task1(self):
-        """Test 1"""
-
-        self.assertEqual(self.b1.id, 1)
-        self.assertEqual(self.b2.id, 2)
-        self.assertEqual(self.b3.id, 3)
-        self.assertEqual(self.b4.id, 5)
-        self.assertEqual(self.b5.id, -5)
-        self.assertEqual(self.b6.id, 42)
-        self.assertEqual(self.b7.id, 4)
-        self.assertEqual(self.b8.id, 5)
-
-        with self.assertRaises(TypeError):
-            b9 = Base(1, 1, 1)
-
-    def test_task15(self):
-        """Test 2"""
-
-        my_dict = {'width': 10, 'height': 7, 'x': 2, 'y': 8, 'id': 1}
-        json_dict = Base.to_json_string([my_dict])
-
-        self.assertEqual(type(my_dict), dict)
-        self.assertEqual(type(json_dict), str)
-        self.assertEqual(my_dict, {'x': 2, 'width': 10, 'id': 1,
-                                   'height': 7, 'y': 8})
-        self.assertEqual(json_dict, '[{"width": 10, "height": 7, \
-"x": 2, "y": 8, "id": 1}]')
-        self.assertEqual(Base.to_json_string([]), "[]")
-        self.assertEqual(Base.to_json_string(None), "[]")
-
-    def test_task17(self):
-        """Test 3"""
-
-        json_str = '[{"width": 10, "height": 7, "x": 2, "y": 8, "id": 1}]'
-        output = Base.from_json_string(json_str)
-        self.assertEqual(output, [{"width": 10, "height": 7,
-                                   "x": 2, "y": 8, "id": 1}])
+class TestBaseMethods(unittest.TestCase):
+    """Testing Base methods"""
 
     def test_pep8_base(self):
-        """Test 4"""
-
+        """
+        Test that models/base.py is pep8 compliant.
+        """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/base.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_test_base(self):
-        """Test 5"""
-
+        """
+        Test that tests/test_models/test_base.py is pep8 compliant
+        """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['tests/test_models/test_base.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_module_docstring(self):
-        """Test 6"""
-
+        """
+        Tests for the module docstring
+        """
         self.assertTrue(len(Base.__doc__) >= 1)
 
     def test_class_docstring(self):
-        """Test 7"""
-
+        """Tests for the Base class docstring"""
         self.assertTrue(len(Base.__doc__) >= 1)
 
     def test_func_docstrings(self):
-        """Tests docstrings functions"""
-
+        """
+        Tests docstrings functions
+        """
         self.assertTrue(len(Base.__init__.__doc__) >= 1)
         self.assertTrue(len(Base.to_json_string.__doc__) >= 1)
         self.assertTrue(len(Base.from_json_string.__doc__) >= 1)
         self.assertTrue(len(Base.save_to_file.__doc__) >= 1)
         self.assertTrue(len(Base.create.__doc__) >= 1)
         self.assertTrue(len(Base.load_from_file.__doc__) >= 1)
+
+    @classmethod
+    def setUpClass(cls):
+        """Sets __nb_object to 0 and creates objects to be tested"""
+        Base.clear()
+        cls.b1 = Base()
+        cls.b2 = Base(12)
+        cls.b3 = Base(-1)
+        cls.b4 = Base(None)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Deletes the objects set up"""
+        del cls.b1
+        del cls.b2
+        del cls.b3
+        del cls.b4
+
+    def test_init(self):
+        """checks base IDs"""
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 12)
+        self.assertEqual(self.b3.id, -1)
+        self.assertEqual(self.b4.id, 2)
+
+        with self.assertRaises(TypeError):
+            b = Base(1, 1)
+
+    def test_to_json_string(self):
+        """tests to_json_string function"""
+        test_dict = {'x': 2, 'y': 8, 'width': 10, 'height': 7}
+        json_dict = Base.to_json_string([test_dict])
+        self.assertEqual(type(json_dict), str)
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(json_dict, '[{"x": 2, "y": 8, "width": \
+10, "height": 7}]')
+
+    def test_from_json_string(self):
+        """Tests from_json_string function"""
+        json_string = '[{"x": 2, "y": 8, "width": 10, "height": 7}]'
+        output = Base.from_json_string(json_string)
+        self.assertEqual(output, [{"x": 2, "y": 8, "width": 10, "height": 7}])
 
 if __name__ == '__main__':
     unittest.main()
