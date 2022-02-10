d63f838275cf09efc3eb97e656516348b062f3ac
Joseph W <3599@holbertonschool.com>
2022-02-10 09:56:41 -0600

    create on one line

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,5 @@
 -- Task 9
 -- Creates second_table
-CREATE TABLE if not EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
+CREATE TABLE if not EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES (id, name, score)
 VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
