8b78a1fc82b2a154c071146240b6ec055308b3f3
3599josephw <3599@holbertonschool.com>
2022-02-14 10:55:29 -0800

    task 11

--- 0x01-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- Task 11: Lists all shows in the database
+-- Task 11: List all shows in the database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
