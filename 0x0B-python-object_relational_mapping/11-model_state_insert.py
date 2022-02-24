61d9324caad91a662d76f3e0b0bbd61087563fad
Joseph W <3599@holbertonschool.com>
2022-02-23 19:33:59 -0600

    bind engine

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
                     sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker()
-    session = Session(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     new = State(name='Louisiana')
 
