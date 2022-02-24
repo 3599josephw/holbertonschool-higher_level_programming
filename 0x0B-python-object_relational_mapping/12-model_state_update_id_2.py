f8b3f5be8729219bb819d5fa056124976aa46124
Joseph W <3599@holbertonschool.com>
2022-02-23 19:37:52 -0600

    added port

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     from sqlalchemy import *
     from sqlalchemy.orm import sessionmaker, Session
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost\
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306\
                         /{}'.format(sys.argv[1], sys.argv[2],
-                                    sys.argv[3]), pool_pre_ping=True)
+                                    sys.argv[3]))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker()
--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker, Session
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost\
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306\
         /{}'.format(sys.argv[1], sys.argv[2],
                     sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
