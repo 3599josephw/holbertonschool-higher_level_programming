aaa56935c4d5651f2063908ab60519db1b998784
Joseph W <3599@holbertonschool.com>
2022-03-08 13:06:02 -0600

    different import statements

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -2,12 +2,12 @@
 """Catches HTTP errors"""
 
 if __name__ == '__main__':
-    from urllib import request, error
+    import urllib.request
     import sys
 
     try:
-        with request.urlopen(sys.argv[1]) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             html = response.read()
             print(html.decode('UTF-8'))
-    except error.HTTPError as e:
+    except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
